e2e780c0ee56b2479a712053fd3b455f20053e41
LuisGuerra05 <luguerra@alumnos.uai.cl>
2024-09-29 22:30:52 -0300

    Ruta de login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import UserList from './components/UserList';
 import './App.css';
 
 function App() {
