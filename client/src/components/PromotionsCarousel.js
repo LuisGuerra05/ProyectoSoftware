e465877439bcbed5e0d8ccb1b7e14411d42cda3b
LuisGuerra05 <luguerra@alumnos.uai.cl>
2024-10-02 14:54:09 -0300

    Cambio de Imagen en HomePage

--- client/src/components/PromotionsCarousel.js
+++ client/src/components/PromotionsCarousel.js
@@ -15,15 +15,15 @@ function PromotionsCarousel() {
     >
       <div>
         <img src="/images/Barca/Local/Barca_Local_24_3.jpg" alt="Promoción 1" />
-        <p className="legend">¡Oferta especial en camisetas del Barcelona! 20% de descuento</p>
+        <p className="legend">¡Solo por tiempo limitado! 20% de descuento en las camisetas oficiales del FC Barcelona. ¡No te lo pierdas!</p>
       </div>
       <div>
-        <img src="/images/Getafe/Local/Getafe_Local_24_4.jpg" alt="Promoción 2" />
-        <p className="legend">Getafe - Camisetas 2x1 por tiempo limitado</p>
+        <img src="/images/Madrid/Madrid_Promocion.jpg" alt="Promoción 2" />
+        <p className="legend">¡Oferta 2x1 en camisetas del Real Madrid! Equípate y comparte con tus amigos. Válido hasta agotar existencias.</p>
       </div>
       <div>
         <img src="/images/Atletico/Local/Atletico_Local_24_3.jpg" alt="Promoción 3" />
-        <p className="legend">Atlético de Madrid: 15% de descuento para miembros</p>
+        <p className="legend">¡Exclusivo para socios! Obtén un 15% de descuento en la nueva camiseta del Atlético de Madrid. ¡Únete a la pasión rojiblanca!</p>
       </div>
     </Carousel>
   );
